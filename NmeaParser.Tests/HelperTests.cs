cb22bd1f4400acaca0892173cefac76ad0b898bf
john <john.demetriou.jd@gmail.com>
2020-07-19 16:00:15 +0300

    Add Lat and Lon Parser
    resolves #26, #27

---
+++ NmeaParser.Tests/HelperTests.cs
@@ -0,0 +1,43 @@
+using Xunit;
+
+namespace NmeaParser.Tests
+{
+    public class HelperTests
+    {
+        [Fact]
+        public void StringToLatitudeValidValueNorth()
+        {
+            var lat = "4917.24";
+            var orientation = "N";
+
+            Assert.Equal(49.287333, Helper.StringToLatitude(lat, orientation), 6);
+        }
+
+        [Fact]
+        public void StringToLatitudeValidValueSouth()
+        {
+            var lat = "4917.24";
+            var orientation = "S";
+
+            Assert.Equal(-49.287333, Helper.StringToLatitude(lat, orientation), 6);
+        }
+        
+        [Fact]
+        public void StringToLongitudeValidValueWest()
+        {
+            var lat = "12309.57";
+            var orientation = "W";
+
+            Assert.Equal(-123.1595, Helper.StringToLongitude(lat, orientation), 6);
+        }
+
+        [Fact]
+        public void StringToLongitudeValidValueEast()
+        {
+            var lat = "12309.57";
+            var orientation = "E";
+
+            Assert.Equal(123.1595, Helper.StringToLongitude(lat, orientation), 6);
+        }
+    }
+}
--- NmeaParser.Tests/NmeaLineManagerTests.cs
+++ NmeaParser.Tests/NmeaLineManagerTests.cs
@@ -1,6 +1,4 @@
-using System;
 using Xunit;
-using NmeaParser;
 
 namespace NmeaParser.Tests
 {
