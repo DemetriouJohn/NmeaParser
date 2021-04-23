3c7fdacce8c3e8594790c59f5d677ead8cb51863
john <john.demetriou.jd@gmail.com>
2021-04-23 15:38:38 +0300

    Refactor enums

---
+++ NmeaParser/NmeaLines/Enums/ModeSelection.cs
@@ -0,0 +1,18 @@
+﻿namespace NmeaParser.NmeaLines.Enums
+{
+    /// <summary>
+    /// Mode selection
+    /// </summary>
+    public enum ModeSelection
+    {
+        /// <summary>
+        /// Automatic, allowed to automatically switch 2D/3D
+        /// </summary>
+        Auto,
+        /// <summary>
+        /// Manual mode
+        /// </summary>
+        Manual,
+
+    }
+}
